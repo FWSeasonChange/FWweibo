226d33ad8df175ebb97481040d8788d28264ec63
付玮 <fw@fuweideMacBook-Pro.local>
2015-08-18 10:55:53 +0800

    Initial Commit

---
+++ 微博01-主框架-01基本环境搭建/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  微博01-主框架-01基本环境搭建
+//
+//  Created by 付玮 on 15/8/18.
+//  Copyright (c) 2015年 付玮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 微博01-主框架-01基本环境搭建/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  微博01-主框架-01基本环境搭建
+//
+//  Created by 付玮 on 15/8/18.
+//  Copyright (c) 2015年 付玮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
